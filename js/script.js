df1138234874de3ad7c7447a6a0b805ad1e9e930
Claudio <claudiodicaprio89@gmail.com>
2021-11-18 15:19:53 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// **************Brainstorming**************
+
+//approccio del value come nell'esercizio del biglietto del treno, se l'utente dovrá eseguire una scelta e in base alla scelta avremo tre diverse quantitá di scatole, sará proprio questo l'elemento da utilizzare per poter modificare il layout tramite la raccolta del value(crea una varaiabile alla quale coleghi il .value, riguarda la lezione commento riguardante lo SWITCH) quindi per ogni livello di difficoltá scelta creeremo delle scatole con un calc diverso uno per beginner un o per warrior e uno per god(CICLO FOR???)
+
+// la griglia andrá prodotta in seguito al click quindi necessiteremo di un add event listner click con una funzione che modificherá il calc, o meglio aggiungerá la classe(a cosa?) 
+
+// dobbiamo creare i div a cui aggiungere la classe e visto che vengono modificati da un calc che viene modificato da una scelta dell'utente tanto vale aggiungerli dinamicamente (let += `` , create element, add element)
+
+//al click avremo anche un altro evento ovvero il cambio di colore dello sfondo (remove class add class )
+
+// COSA PUÓ ESSER FATTO CON UNA FUNZIONE?
+// RIVEDI GEEKSFORGEEKS NESTED SWITCH,NESTED FUNCTION.
+// UN IF PUÓ CONTENERE UNA FUNZIONE.
+
+// *********reminder*********
+
+// ricorda diapplicare il choose your destiny con js altrimenti fará sfalzare i div in basso se non twikki la visibilitá
+
+
+
+
+
+
