d9ed5552352e3adde10feffc496f720f5ebc0db0
Claudio <claudiodicaprio89@gmail.com>
2021-11-18 15:31:55 +0100

    function

--- js/script.js
+++ js/script.js
@@ -21,3 +21,59 @@
 
 
 
+// *********FUNCTION*********
+
+// 1 GENERATE DIV AND APPEND THEM
+// 2 GIVES BACK A NUMBER BASED ON DIFFICULTY VALUE
+// 3 MAIN BRINGS SQUARE TO LIFE
+// 1
+function SquareMaster(create) {
+   
+    const square = document.createElement('div');
+    create.append(square);
+    return square;
+ }
+
+//  2
+function howMany(rank) {
+   
+    if (rank == 'beginner') {
+ 
+       return 100;
+    } else if (rank == 'warrior') {
+       
+       return 81;
+    }
+ 
+    return 49;
+ }
+
+ function Grid(value) {
+   
+    // VALUE WILL BE REPLACE BY THE VALUE THAT I'LL GET AFTER THE  CHOICE WITH THE HELP OF A VARIABLE
+    for (let i = 0; i < value; i++) {
+
+       const square = SquareMaster(tatami);
+       square.className = ('square');
+       square.addEventListener("click" , function(){
+          square.classList.add('bmk');
+       });
+      
+        //THIS SWITCH ADDS THE CLASS BASED ON THE VALUE RETURNED FROM THE CHOICE    
+       switch (value) {
+          case 100:
+             square.classList.add('beginner');
+             break;
+          case 81:
+             square.classList.add('warrior');
+             break; 
+          case 49:
+             square.classList.add('god');
+             break;      
+       }
+    
+       let x = square.innerHTML = (i+1);
+    }
+ }
+
+ 
